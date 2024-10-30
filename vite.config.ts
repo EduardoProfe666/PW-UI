4721c5072f9a909a8728b3eb85b967297de7539d
Eduardo Alejandro González Martell <eduardoglez64377@gmail.com>
2024-10-30 17:14:41 -0400

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -20,7 +20,5 @@ export default defineConfig({
       include: resolve(dirname(fileURLToPath(import.meta.url)), './src/i18n/locales/**'),
     }),
   ],
-  base: process.env.NODE_ENV === 'production'
-    ? '/pw-ui/'
-    : '/'
+  base: process.env.NODE_ENV === '/pw-ui/',
 })
